b7461da6418ec570c433432490d5ed34259d847d
“dan-gheorghita” <dan.gheorghita@student.upt.ro>
2021-07-22 17:16:40 +0300

    Full program

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-This is a git repo
+This program checks for files in a directory then splits them into another directory. After splitting the file it reasambles in the same path.
+
+othercheckdir.cpp is the main program
+checkdir contains the cmake build files
--- otherreaddir.cpp
+++ otherreaddir.cpp
@@ -1,218 +1,201 @@
-#include <unistd.h>
-#include <iostream>
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <string.h>
-#include <cstdlib>
-#include <cstdio>
-#include <ctime>
-#include <thread>
 #include <chrono>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <dirent.h>
 #include <fstream>
+#include <iostream>
+#include <string.h>
 #include <string>
-
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <thread>
+#include <unistd.h>
 
 using namespace std;
 
-char **sir = (char **)malloc(10 * sizeof(char *));
+char** sir = (char**)malloc(10 * sizeof(char*));
 time_t timesir[10];
 
 int change = 0;
 int check = 0;
 
-int find_file(const char *nm)
+int find_file(const char* nm)
 {
-        int i;
-        for(i = 0; i < change ; i++)
-                        if( strcmp(*(sir +i),nm) == NULL )
-                        {
-                        //cout << nm << ' ';
-                        return i+1;
-                        }
-        return 0;    
+    int i;
+    for (i = 0; i < change; i++)
+        if (strcmp(*(sir + i), nm) == NULL) {
+            return i + 1;
+        }
+    return 0;
 }
 
 void stergere(int k)
 {
-	int i;
-	for(i = k; i < change - 1; i++)
-	{
-		strcpy(*(sir + i),*(sir + i+1));
-		timesir[i] = timesir[i+1];
-	}
-	
-	free(*(sir + change - 1));
+    int i;
+    for (i = k; i < change - 1; i++) {
+        strcpy(*(sir + i), *(sir + i + 1));
+        timesir[i] = timesir[i + 1];
+    }
+
+    free(*(sir + change - 1));
 }
 
 void dividerestore(string dir, string fullnume)
 {
-  
-  string fout (dir);
-  fout += "bufferdir/";
 
-  int i = 0;
-  int length = 0;
-  for(i = fullnume.length() - 1; i >= 0; i--)
-    if(fullnume[i] == '.')
-      break;
-    cout << i << " " << fullnume.length() - 1 << '\n';
-  char c_nume[20];
-  char c_ext[10];
-  length = fullnume.copy(c_nume,i,0);
-  c_nume[length] = '\0';
-  length = fullnume.copy(c_ext,fullnume.length() - 1,i);
-  c_ext[length] = '\0';
-  cout << c_nume << c_ext << '\n';
-  string nume(c_nume);
-  string ext(c_ext);
+    string fout(dir);
+    fout += "bufferdir/";
 
-  ifstream finfile (dir + fullnume,ifstream::binary);
-  ofstream foutfile (fout + nume + "_final" + ext,ofstream::binary);
+    int i = 0;
+    int length = 0;
+    //cautarea punctului pentru a separa extensia de nume
+    for (i = fullnume.length() - 1; i >= 0; i--)
+        if (fullnume[i] == '.')
+            break;
+    //cout << i << " " << fullnume.length() - 1 << '\n';
 
-  long size = 1000;
+    char c_nume[20];
+    char c_ext[10];
+    length = fullnume.copy(c_nume, i, 0);
+    c_nume[length] = '\0';
+    length = fullnume.copy(c_ext, fullnume.length() - 1, i);
+    c_ext[length] = '\0';
 
-  i=0;
+    //cout << c_nume << c_ext << '\n';
+    string nume(c_nume);
+    string ext(c_ext);
 
-  while(finfile.eof() == false)
-  {
+    ifstream finfile(dir + fullnume, ifstream::binary);
+    ofstream foutfile(fout + nume + "_final" + ext, ofstream::binary);
 
-  ofstream outfile (fout + nume + "_" + to_string(i) + ".txt",ofstream::binary);
-  char* buffer = new char[size];
+    long size = 1000; //marimea fisierelor de 1KB
 
-  finfile.read (buffer,size);
+    i = 0;
+    //crearea fisierelor mici
+    while (finfile.eof() == false) {
 
-  outfile.write (buffer,size);
+        ofstream outfile(fout + nume + "_" + to_string(i) + ".txt",ofstream::binary);
 
-  delete[] buffer;
+        char* buffer = new char[size];
 
-  outfile.close();
+        finfile.read(buffer, size);
 
-  i++;
+        outfile.write(buffer, size);
 
-  }
-  int sup = i;
-  i = 0;
-  while(i<sup)
-  {
+        delete[] buffer;
 
-  ifstream infile (fout + nume + "_" + to_string(i) + ".txt",ofstream::binary);
-  char* buffer = new char[size];
+        outfile.close();
 
-  infile.read (buffer,size);
+        i++;
+    }
+    int sup = i;
+    i = 0;
+    
+    //restaurarea fisierului principal
+    while (i < sup) {
 
-  foutfile.write (buffer,size);
+        ifstream infile(fout + nume + "_" + to_string(i) + ".txt",ofstream::binary);
 
-  delete[] buffer;
+        char* buffer = new char[size];
 
-  infile.close();
+        infile.read(buffer, size);
 
-  i++;
-  
-  }
+        foutfile.write(buffer, size);
 
-  foutfile.close();
-  finfile.close();
+        delete[] buffer;
 
+        infile.close();
+
+        i++;
+    }
+
+    foutfile.close();
+    finfile.close();
 }
 
-int check_dir(const char *dir)
+int check_dir(const char* dir)
 {
- 	
 
-	if (chdir(dir) == -1){
-		perror(dir);
-		return 1;
-	}
+    if (chdir(dir) == -1) { //schimbarea cu directorul apelat
+        perror(dir);
+        return 1;
+    }
 
-	DIR *dirp = opendir(".");
-	
-	struct dirent *dent;
+    DIR* dirp = opendir(".");
 
-	while( (dent = readdir(dirp)) != NULL )
-	{
-		char *nm = dent->d_name;
+    struct dirent* dent;
 
-		if (strcmp(nm, ".") == 0 || strcmp(nm, "..") == 0)
-	            continue;
+    while ((dent = readdir(dirp)) != NULL) { // citirea din director
+        char* nm = dent->d_name;
 
-	    	//cout << nm << ' ';
+        if (strcmp(nm, ".") == 0 || strcmp(nm, "..") == 0)
+            continue;
 
-	    	if ( check == 0 )
-	    	{
-	            	if (mkdir("bufferdir", 0777) == -1)
-	            	{
-        			cerr << "Error :  " << strerror(errno) << endl;
-  		    	}
-    		    	else
-    		    	{
-    		    		cout << "Directory created" << endl;
-    		    	}
-       			check = 1;
-	    	}
-	    	else if (strcmp(nm, "bufferdir") == 0)
-	            	continue;
+        if (check == 0) { //crearea directorului pentru transfer
+            if (mkdir("bufferdir", 0777) == -1) {
+                cerr << "Error :  " << strerror(errno) << endl;
+            } else {
+                cout << "Directory created" << endl;
+            }
+            check = 1;
+        } else if (strcmp(nm, "bufferdir") == 0)
+            continue;
 
-	    	int var = find_file(nm);
-	    	
-		if(var)
-		{
-			time_t timer;
-			time(&timer);
-			//check time(nm);
+        int var = find_file(nm); //cautarea fisierului in vectorul cu denumirile
 
-			if( difftime(timer,timesir[var-1]) >= 15 ) //time(nm)>10s
-			{ 
-				//transfera fisieru in 1KB
-				string st_dir(dir);
-				string st_nm(nm);
-				dividerestore(st_dir,st_nm);
-				printf("\n%s | curent poz in sir:%d max poz:%d\n", nm, var-1, change);
-				
-				if( remove(nm) != 0 )
-    					perror( "Error deleting file" );
-  				else
-    					puts( "File successfully deleted" );
+        if (var) {
+        	  // check curent time
+            time_t timer;
+            time(&timer);
 
-				stergere(var-1);
-				change--;
-			}
-			
-			
-			
-			
-		}
-		else
-		{
-			cout << nm << ' ';
-			
-			//*(sir + change) = nm;
-			*(sir + change) = (char *)malloc(strlen(nm) + 1);
-			strcpy(*(sir + change),(char *)nm);
+            if (difftime(timer, timesir[var - 1]) >= 10) // timpul fisierului in folder: time(nm)>10s
+            {
+                // transfera fisieru in fisiere 1KB si lea saambleaza la loc
+                string st_dir(dir);
+                string st_nm(nm);
+                dividerestore(st_dir, st_nm);
 
-			//set time(nm);
-			timesir[change] = time(NULL);
+                printf("\n%s | curent poz in sir:%d max poz:%d\n", nm, var - 1, change);
 
-			//timesir = (time_t *) realloc (timesir, change * sizeof(time_t));
-			//*(timesir + change) = time(NULL);
-			change++;
-		}
-		
-	}
-	//cout << '\n';
-	closedir(dirp);
-	
-	return 0;
+                //stergerea fisierlui initial
+                if (remove(nm) != 0)
+                    perror("Error deleting file");
+                else
+                    puts("File successfully deleted");
+
+                //stregerea in vectori
+                stergere(var - 1);
+                change--;
+            }
+
+        } else {
+            cout << nm << ' ';
+            
+            //*(sir + change) = nm;
+            *(sir + change) = (char*)malloc(strlen(nm) + 1);
+            strcpy(*(sir + change), nm);
+
+            // set time(nm);
+            timesir[change] = time(NULL);
+
+            
+            
+            change++;
+        }
+    }
+    // cout << '\n';
+    closedir(dirp);
+
+    return 0;
 }
 
 int main()
 {
-	
-	while(1)
-	{
-		this_thread::sleep_for (chrono::milliseconds(500));
-		check_dir("/home/ban/Desktop/pics/");
-		
-	}
-	return(0);
+
+    while (1) {
+        this_thread::sleep_for(chrono::milliseconds(500));
+        check_dir("/home/ban/Desktop/pics/");
+    }
+    return (0);
 }
