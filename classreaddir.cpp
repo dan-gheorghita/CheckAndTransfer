136d0f024e9d8b00f3e9bae1c0eebe82a7566a55
“dan-gheorghita” <dan.gheorghita@student.upt.ro>
2021-07-27 03:49:04 +0300

    Programul cu clase

--- classreaddir.cpp
+++ classreaddir.cpp
@@ -20,28 +20,26 @@ time_t timesir[10];
 int change = 0;
 int check = 0;
 
-class Directory
-{
+class Directory {
     const char* dir;
-    public:
-        Directory (const char* x) {dir = x;}
-        void afisare ();
-        int check_dir();
+
+public:
+    Directory(const char* x) { dir = x; }
+    int check_dir();
 };
 
-class SplitFile
-{
+class SplitFile {
     string st_dir;
     string st_nm;
-    public:
-        SplitFile (const char* x, const char* y) {st_dir = x; st_nm = y;}
-        void dividerestore();
-};
 
-void Directory::afisare ()
-{
-    cout << dir << '\n';
-}
+public:
+    SplitFile(const char* x, const char* y)
+    {
+        st_dir = x;
+        st_nm = y;
+    }
+    void dividerestore();
+};
 
 int find_file(const char* nm)
 {
@@ -66,17 +64,16 @@ void stergere(int k)
 
 void SplitFile::dividerestore()
 {
-
     string fout(st_dir);
     fout += "bufferdir/";
 
     int i = 0;
     int length = 0;
-    //cautarea punctului pentru a separa extensia de nume
+    // cautarea punctului pentru a separa extensia de nume
     for (i = st_nm.length() - 1; i >= 0; i--)
         if (st_nm[i] == '.')
             break;
-    //cout << i << " " << st_nm.length() - 1 << '\n';
+    // cout << i << " " << st_nm.length() - 1 << '\n';
 
     char c_nume[20];
     char c_ext[10];
@@ -85,38 +82,33 @@ void SplitFile::dividerestore()
     length = st_nm.copy(c_ext, st_nm.length() - 1, i);
     c_ext[length] = '\0';
 
-    //cout << c_nume << c_ext << '\n';
+    // cout << c_nume << c_ext << '\n';
     string nume(c_nume);
     string ext(c_ext);
 
     struct stat file_stats;
-        if (stat(st_nm.c_str(), &file_stats) == -1)
-            perror(st_nm.c_str());
-
-        else
-            printf("\n%9u: %s ", (unsigned)file_stats.st_size, st_nm.c_str());
-
-
+    if (stat(st_nm.c_str(), &file_stats) == -1)
+        perror(st_nm.c_str());
 
     ifstream finfile(st_dir + st_nm, ifstream::binary);
-    //ofstream foutfile(fout + nume + "_final" + ext, ofstream::binary);
+    // ofstream foutfile(fout + nume + "_final" + ext, ofstream::binary);
 
-    int size = 1000; //marimea fisierelor de 1KB
+    int size = 1000; // marimea fisierelor de 1KB
 
+    // fixeaza citirea pe biti, care inainte adauga biti nuli
     unsigned long filesize = (unsigned long)file_stats.st_size;
-    cout << filesize << " ";
-    double middle= (double)filesize / size;
-    cout << middle << " ";
-    unsigned long maxim = (unsigned long) middle;
-    cout << maxim << " ";
+    //cout << filesize << " ";
+    double middle = (double)filesize / size;
+    //cout << middle << " ";
+    unsigned long maxim = (unsigned long)middle;
+    //cout << maxim << " ";
     int last = (int)((middle - maxim) * size);
-    cout << last << " ";
+    //cout << last << " ";
 
     i = 0;
-    //crearea fisierelor mici
+    // crearea fisierelor mici
     while (i < maxim) {
-
-        ofstream outfile(fout + nume + "_" + to_string(i) + ".txt",ofstream::binary);
+        ofstream outfile(fout + nume + "_" + to_string(i) + ".txt", ofstream::binary);
 
         char* buffer = new char[size];
 
@@ -131,9 +123,8 @@ void SplitFile::dividerestore()
         i++;
     }
 
-    if (last)
-    {
-        ofstream outfile(fout + nume + "_" + to_string(i) + ".txt",ofstream::binary);
+    if (last) {
+        ofstream outfile(fout + nume + "_" + to_string(i) + ".txt", ofstream::binary);
 
         char* buffer = new char[last];
 
@@ -144,17 +135,15 @@ void SplitFile::dividerestore()
         delete[] buffer;
 
         outfile.close();
-
     }
-    
-    //foutfile.close();
+
+    // foutfile.close();
     finfile.close();
 }
 
-int Directory::check_dir ()
+int Directory::check_dir()
 {
-
-    if (chdir(dir) == -1) { //schimbarea cu directorul apelat
+    if (chdir(dir) == -1) { // schimbarea cu directorul apelat
         perror(dir);
         return 1;
     }
@@ -169,7 +158,7 @@ int Directory::check_dir ()
         if (strcmp(nm, ".") == 0 || strcmp(nm, "..") == 0)
             continue;
 
-        if (check == 0) { //crearea directorului pentru transfer
+        if (check == 0) { // crearea directorului pentru transfer
             if (mkdir("bufferdir", 0777) == -1) {
                 cerr << "Error :  " << strerror(errno) << endl;
             } else {
@@ -179,41 +168,41 @@ int Directory::check_dir ()
         } else if (strcmp(nm, "bufferdir") == 0)
             continue;
 
-        int var = find_file(nm); //cautarea fisierului in vectorul cu denumirile
+        int var = find_file(nm); // cautarea fisierului in vectorul cu denumirile
 
         if (var) {
-        	  // check curent time
+            // check curent time
             time_t timer;
             time(&timer);
 
             if (difftime(timer, timesir[var - 1]) >= 10) // timpul fisierului in folder: time(nm)>10s
             {
                 /*string st_dir(dir);
-                string st_nm(nm);
-                dividerestore(st_dir, st_nm);*/
+        string st_nm(nm);
+        dividerestore(st_dir, st_nm);*/
 
                 // transfera fisieru in fisiere 1KB si lea saambleaza la loc
 
-                SplitFile *file = new SplitFile(dir,nm);
+                SplitFile* file = new SplitFile(dir, nm);
                 file->dividerestore();
                 delete file;
 
                 printf("\n%s | curent poz in sir:%d max poz:%d\n", nm, var - 1, change);
 
-                //stergerea fisierlui initial
+                // stergerea fisierlui initial
                 if (remove(nm) != 0)
                     perror("Error deleting file");
                 else
                     puts("File successfully deleted");
 
-                //stregerea in vectori
+                // stregerea in vectori
                 stergere(var - 1);
                 change--;
             }
 
         } else {
             cout << nm << ' ';
-            
+
             //*(sir + change) = nm;
             *(sir + change) = (char*)malloc(strlen(nm) + 1);
             strcpy(*(sir + change), nm);
@@ -221,8 +210,6 @@ int Directory::check_dir ()
             // set time(nm);
             timesir[change] = time(NULL);
 
-            
-            
             change++;
         }
     }
